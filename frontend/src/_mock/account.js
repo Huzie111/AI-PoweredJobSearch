8dd1f517568b5c390d79f937e8faf52cb1d99881
Huzie111 <110819348+Huzie111@users.noreply.github.com>
2026-09-05 08:22:30 +0300

    Update account profile with user information

--- frontend/src/_mock/account.js
+++ frontend/src/_mock/account.js
@@ -1,7 +1,8 @@
 // ----------------------------------------------------------------------
 
 export const account = {
-  displayName: 'Jaydon Frankie',
-  email: 'demo@minimals.cc',
-  photoURL: '/assets/images/avatars/avatar_25.jpg',
+  displayName: 'Serunjogi Huzaifa',
+  email: 'serunjogi@example.com',
+  photoURL: '/assets/images/avatars/profile.jpg',
+  role: 'Professional',
 };
